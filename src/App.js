9c66d40103d390a6ef320e972269b8f41dd729ab
tojibayevvv <abdullohspersonaldevelopment.28@gmail.com>
2026-05-23 11:18:06 +0500

    refactor: pass movies data to Main, ListBox, and MovieList components

--- src/App.js
+++ src/App.js
@@ -51,10 +51,12 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+
   return (
     <>
       <NavBar />
-      <Main />
+      <Main movies={movies}/>
     </>
   );
 }
@@ -100,16 +102,16 @@ function NumResults() {
   );
 }
 
-function Main() {
+function Main({movies}) {
   return (
     <main className="main">
-      <ListBox />
+      <ListBox movies={movies}/>
       <WatchedBox />
     </main>
   );
 }
 
-function ListBox() {
+function ListBox({movies}) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -120,13 +122,12 @@ function ListBox() {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList />}
+      {isOpen1 && <MovieList movies={movies}/>}
     </div>
   );
 }
 
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({movies}) {
 
   return (
     <ul className="list">
