2b6e2dcb0688bd926d4d7973d03688220b89aae5
tojibayevvv <abdullohspersonaldevelopment.28@gmail.com>
2026-07-02 15:39:18 +0500

    StarRating maxxing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import StarRating from './StarRating';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
 // import './index.css';
 // import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        maxRating={5}
+        color="green"
+        size={25}
+        defaultRating={3}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
-  </React.StrictMode>
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Perfect"]}
+    />
+    <Test/>
+  </React.StrictMode>,
 );
